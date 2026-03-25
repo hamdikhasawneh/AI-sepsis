09075ac52fbba6b8e7ac5d40b79402f6b64892e1
hamdikhasawneh <hrhkhasawneh@gmail.com>
2026-03-25 23:24:55 +0300

    Update database.py
    update comment

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
-
+# loads database connection string from a .env file and sets up a reusable SQLAlchemy connection/session so the FastAPI app can safely interact with the database.
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
